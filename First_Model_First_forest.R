090e3e4ff360fb702a13a1053febe6f61cc668d8
aashraf451 <54647702+aashraf451@users.noreply.github.com>
2020-02-04 17:02:35 +0200

    Add files via upload

---
+++ First_Model_First_forest.R
@@ -0,0 +1,63 @@
+#Packages, Libraries, Seed
+
+#Installing packages, loading libraries, and setting the seed for reproduceability:
+  
+install.packages("caret")
+install.packages("randomForest")
+install.packages("rpart")
+library(caret)
+library(randomForest)
+library(rpart) # Regressive Partitioning and Regression trees
+library(rpart.plot) # Decision Tree plot
+
+# setting the overall seed for reproduceability
+set.seed(1234)
+
+# After saving both data sets into my working directory
+# Some missing values are coded as string "#DIV/0!" or "" or "NA" - these will be changed to NA.
+# We notice that both data sets contain columns with all missing values - these will be deleted.  
+
+# Loading the training data set into my R session replacing all missing with "NA"
+trainingset <- read.csv("C:/Users/Sandrine/ML_Project/trainingdata.csv", na.strings=c("NA","#DIV/0!", ""))
+
+# Loading the testing data set 
+testingset <- read.csv('C:/Users/Sandrine/ML_Project/testingdata.csv', na.strings=c("NA","#DIV/0!", ""))
+
+# Check dimensions for number of variables and number of observations
+dim(trainingset)
+dim(testingset)
+
+# Delete columns with all missing values
+trainingset<-trainingset[,colSums(is.na(trainingset)) == 0]
+testingset <-testingset[,colSums(is.na(testingset)) == 0]
+
+# Some variables are irrelevant to our current project: user_name, raw_timestamp_part_1, raw_timestamp_part_,2 cvtd_timestamp, new_window, and  num_window (columns 1 to 7). We can delete these variables.
+trainingset   <-trainingset[,-c(1:7)]
+testingset <-testingset[,-c(1:7)]
+
+# and have a look at our new datasets:
+dim(trainingset)
+dim(testingset)
+head(trainingset)
+head(testingset)
+
+#Partitioning the training data set to allow cross-validation
+
+subsamples <- createDataPartition(y=trainingset$classe, p=0.75, list=FALSE)
+subTraining <- trainingset[subsamples, ] 
+subTesting <- trainingset[-subsamples, ]
+dim(subTraining)
+dim(subTesting)
+head(subTraining)
+head(subTesting)
+
+#Data Analysis
+plot(subTraining$classe, col="blue", main="Bar Plot of levels of the variable classe within the subTraining data set", xlab="classe levels", ylab="Frequency")
+#model 2:Random First
+model2 <- randomForest(classe ~. , data=subTraining, method="class")
+
+# Predicting:
+prediction2 <- predict(model2, subTesting, type = "class")
+
+# Test results on subTesting data set:
+confusionMatrix(prediction2, subTesting$classe)
